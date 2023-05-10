8d7e92041a7ea38530539d355ac0beef9ec3b4be
JMKangas <80038481+JMKangas@users.noreply.github.com>
2023-05-10 21:43:42 +0300

    taking a pic /w camera and storing it frontend working

--- my_favorite_place/lib/widgets/image_input.dart
+++ my_favorite_place/lib/widgets/image_input.dart
@@ -1,17 +1,52 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({Key? key}) : super(key: key);
 
   @override
-  State<ImageInput> createState() => _ImageInputState();
+  State<ImageInput> createState() {
+    return _ImageInputState();
+  }
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {}
+  File? _selectedImage;
+
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+
+    if (pickedImage == null) {
+      return;
+    }
+    //final pickedImageFile = pickedImage as File;
+
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      onPressed: _takePicture,
+      icon: const Icon(Icons.camera),
+      label: const Text('Take Picture'),
+    );
+    if (_selectedImage != null) {
+      content = GestureDetector(
+        onTap: _takePicture,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+        ),
+      );
+    }
+
     return Container(
       decoration: BoxDecoration(
         border: Border.all(
@@ -22,13 +57,7 @@ class _ImageInputState extends State<ImageInput> {
       height: 250,
       width: double.infinity,
       alignment: Alignment.center,
-      child: TextButton.icon(
-        onPressed: _takePicture,
-        icon: const Icon(Icons.camera),
-        label: const Text('Take Picture'),
-        //style: TextButton.styleFrom(
-        //primary: Theme.of(context).colorScheme.onBackground,
-      ),
+      child: content,
     );
   }
 }
